7b803f35f0366b9bf2895c8f4ba2981f8373b1c5
tinnamaan <131854078+tinnamaan02@users.noreply.github.com>
2023-10-13 03:13:44 +0800

    Edited index, script, and style file.
    Added a new feature using emailjs.

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs
+    .sendForm(
+      "tintin",
+      "template_7949vtk",
+      "#contact-form",
+      "jEtsatNQF-FLJBevW"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "Sent!"; //if the message you want was sent
+
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        contactForm.reset();
+      },
+      () => {
+        contactMessage.textContent = "Not Sent, Try Again."; //lalabas if may mali sa code on line 16-19
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
